067e5d6b5ada309057a0c6d0c6ae87ec34c7a7a7
samuel_sanderson <sandersonsamuel401@gmail.com>
2024-03-29 12:06:31 -0300

    feat: aumento do limite de caracteres

--- src/components/Modals/ModalAva.jsx
+++ src/components/Modals/ModalAva.jsx
@@ -1,63 +1,68 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { AppRating } from "../Rating";
 import { Modal, Button, ModalBody, Label, Textarea } from "flowbite-react";
 import { auth } from "../../configs/firebase";
 import { useNavigate } from "react-router-dom";
-import { deleteField, doc, getDoc, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
+import {
+  deleteField,
+  doc,
+  getDoc,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { databaseApp } from "../../configs/firebase";
 import { Timestamp } from "firebase/firestore";
-import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { GetAvalAlbum } from "../GetAval";
 
+export function ModalAva({ albumInfos, alerta }) {
+  const navigate = useNavigate();
 
-export function ModalAva({albumInfos, alerta}){
-
-  const navigate = useNavigate()
-
-  const [open, setOpen] = useState(false)
-  const [textAva, setTextAva] = useState("")
-  const [starAva, setStarAva] = useState(null)
+  const [open, setOpen] = useState(false);
+  const [textAva, setTextAva] = useState("");
+  const [starAva, setStarAva] = useState(null);
   const [avaliacoesUser, setAvaliacoesUser] = useState(null);
   const [mscAvaliadas, setMscAvaliadas] = useState([]);
   const [pdAvaliar, setPdAvaliar] = useState(false);
-  const [totalMsc, setTotalMsc] = useState(albumInfos.tracks.items.length)
+  const [totalMsc, setTotalMsc] = useState(albumInfos.tracks.items.length);
 
-  useEffect(()=>{
-    let notas = 0
-
-    if (mscAvaliadas) {
-      mscAvaliadas.map((musica)=>{
-        notas += musica.nota
-      })
-    }
-
-    notas = notas/totalMsc
-
-    setStarAva(notas.toFixed(2))
-
-  },[mscAvaliadas])
+  const limiteCaracteres = 450;
 
   useEffect(() => {
-    if (mscAvaliadas.length === albumInfos.tracks.items.length){
+    let notas = 0;
+
+    if (mscAvaliadas) {
+      mscAvaliadas.map((musica) => {
+        notas += musica.nota;
+      });
+    }
+
+    notas = notas / totalMsc;
+
+    setStarAva(notas.toFixed(2));
+  }, [mscAvaliadas]);
+
+  useEffect(() => {
+    if (mscAvaliadas.length === albumInfos.tracks.items.length) {
       setPdAvaliar(true);
     } else {
       setPdAvaliar(false);
     }
-  }, [mscAvaliadas, albumInfos.tracks.items.length])
+  }, [mscAvaliadas, albumInfos.tracks.items.length]);
 
   useEffect(() => {
-    let avas = []
+    let avas = [];
 
     if (avaliacoesUser) {
-      Object.values(avaliacoesUser).forEach(musica => {
-        if (musica.idAlbum === albumInfos.id){
+      Object.values(avaliacoesUser).forEach((musica) => {
+        if (musica.idAlbum === albumInfos.id) {
           avas.push(musica);
         }
       });
     }
 
-    setMscAvaliadas(avas)
-
+    setMscAvaliadas(avas);
   }, [avaliacoesUser]);
 
   useEffect(() => {
@@ -65,41 +70,40 @@ export function ModalAva({albumInfos, alerta}){
       return;
     }
 
-    const unsubscribe = onSnapshot(doc(databaseApp, `avaliacoes/${auth.currentUser.uid}`), (docSnap) => {
-      if (docSnap.exists()) {
-        setAvaliacoesUser(docSnap.data().musicas);
+    const unsubscribe = onSnapshot(
+      doc(databaseApp, `avaliacoes/${auth.currentUser.uid}`),
+      (docSnap) => {
+        if (docSnap.exists()) {
+          setAvaliacoesUser(docSnap.data().musicas);
+        }
       }
-    });
+    );
 
     return () => unsubscribe();
   }, [auth.currentUser]);
 
-
   const handleOpen = () => {
     if (!auth.currentUser) {
-      navigate(`/login`)
+      navigate(`/login`);
     }
 
-    if(pdAvaliar){
-      setOpen(true)
-    }else{
-      alerta()
+    if (pdAvaliar) {
+      setOpen(true);
+    } else {
+      alerta();
     }
-    
-  }
-  const handleClose = () => setOpen(false)
+  };
+  const handleClose = () => setOpen(false);
 
   const handleSubmit = async () => {
+    handleClose();
 
-    handleClose()
+    const db = databaseApp;
+    const userDoc = doc(db, "avaliacoes", auth.currentUser.uid);
 
-    const db = databaseApp
-    const userDoc = doc(db, 'avaliacoes', auth.currentUser.uid);
-    
-    const docSnap = await getDoc(userDoc)
-  
+    const docSnap = await getDoc(userDoc);
+
     if (!docSnap.exists()) {
-
       await setDoc(userDoc, {
         albuns: {
           [`${albumInfos.id}`]: {
@@ -109,9 +113,9 @@ export function ModalAva({albumInfos, alerta}){
             avaliacao: textAva,
             nameAlbum: albumInfos.name,
             dataAva: Timestamp.now(),
-            InfoAlbum: albumInfos
-          }
-        }
+            InfoAlbum: albumInfos,
+          },
+        },
       });
     } else {
       // Se o usuário já existir, só atualiza os dados
@@ -123,93 +127,123 @@ export function ModalAva({albumInfos, alerta}){
           avaliacao: textAva,
           nameAlbum: albumInfos.name,
           dataAva: Timestamp.now(),
-          InfoAlbum: albumInfos
-        }
+          InfoAlbum: albumInfos,
+        },
       });
     }
-  }
-
-  return(
-    <>
-      <i onClick={handleOpen} className="fa-solid fa-pen-to-square text-2xl md:text-3xl cursor-pointer"></i>
-        <Modal dismissible show={open} onClose={handleClose} >
-          <Modal.Header>Avaliar "{albumInfos.name}"</Modal.Header>
-          <ModalBody>
-          <div className="space-y-6">
-
-              <AppRating value={starAva} />
-
-              <div className="max-w-md">
-                <div className="mb-2 block">
-                  <Label htmlFor="comment" value="Sua avaliação" />
-                </div>
-                <Textarea onChange={(e)=> setTextAva(e.target.value)} id="comment" placeholder="Avalie aqui..." required rows={4} />
-              </div>
-
-              <div className={`w-12 border-2 ${350 - textAva.length <= 25 ? 'border-red-500 text-red-500' : 350 - textAva.length > 25 && 350 - textAva.length <=50 ? 'border-yellow-400 text-yellow-400': 'border-green-500 text-green-500'} flex items-center justify-center rounded-full`}>
-                <p className="">{350 - textAva.length}</p>
-              </div>
-
-            </div>
-          </ModalBody>
-          <Modal.Footer>
-            <div className="flex w-full justify-between gap-1">
-              <div className="flex gap-1 md:gap-2">
-                <Button size={'sm'} onClick={handleSubmit} disabled={textAva.length > 350}>Enviar</Button>
-                <Button size={'sm'} color="gray" onClick={handleClose}>Cancelar</Button>
-              </div>
-              <AlertDeleteAva setClodeModal={setOpen} albumInfos={albumInfos}/>
-            </div>
-          </Modal.Footer>
-
-        </Modal>
-    </>
-  )
-}
-
-
-function AlertDeleteAva({setClodeModal, albumInfos}) {
-
-  const [openModal, setOpenModal] = useState(false)
-  const [avaliacoes, setAvaliacoes] = useState(false)
-  const [isAva, setIsAva] = useState(false)
-
-  useEffect(()=>{
-    GetAvalAlbum(setAvaliacoes)
-  }, [])
-
-  useEffect(()=>{
-    if (avaliacoes){
-      Object.values(avaliacoes).forEach((avaliacao)=> {
-        if (avaliacao.idAlbum === albumInfos.id){
-          setIsAva(true)
-        }
-      })
-    }
-  }, [avaliacoes])
-
-  const handleDeletAva = async () =>{
-
-    setOpenModal(false)
-
-    const db = databaseApp
-    const userDoc = doc(db, 'avaliacoes', auth.currentUser.uid)
-
-    await updateDoc(userDoc, {
-      [`albuns.${albumInfos.id}`]: deleteField()
-    })
-
-    await setClodeModal(false)
-  }
+  };
 
   return (
     <>
-      {isAva && 
-        <>
+      <i
+        onClick={handleOpen}
+        className="fa-solid fa-pen-to-square text-2xl md:text-3xl cursor-pointer"
+      ></i>
+      <Modal dismissible show={open} onClose={handleClose}>
+        <Modal.Header>Avaliar "{albumInfos.name}"</Modal.Header>
+        <ModalBody>
+          <div className="space-y-6">
+            <AppRating value={starAva} />
 
-          <Button size={'sm'} color="failure" onClick={()=> setOpenModal(true)}>Detelar</Button>
-          
-          <Modal show={openModal} size="md" onClose={() => setOpenModal(false)} popup>
+            <div className="max-w-md">
+              <div className="mb-2 block">
+                <Label htmlFor="comment" value="Sua avaliação" />
+              </div>
+              <Textarea
+                onChange={(e) => setTextAva(e.target.value)}
+                id="comment"
+                placeholder="Avalie aqui..."
+                required
+                rows={4}
+              />
+            </div>
+
+            <div
+              className={`w-12 border-2 ${
+                limiteCaracteres - textAva.length <= 25
+                  ? "border-red-500 text-red-500"
+                  : limiteCaracteres - textAva.length > 25 &&
+                    limiteCaracteres - textAva.length <= 50
+                  ? "border-yellow-400 text-yellow-400"
+                  : "border-green-500 text-green-500"
+              } flex items-center justify-center rounded-full`}
+            >
+              <p className="">{limiteCaracteres - textAva.length}</p>
+            </div>
+          </div>
+        </ModalBody>
+        <Modal.Footer>
+          <div className="flex w-full justify-between gap-1">
+            <div className="flex gap-1 md:gap-2">
+              <Button
+                size={"sm"}
+                onClick={handleSubmit}
+                disabled={textAva.length > limiteCaracteres}
+              >
+                Enviar
+              </Button>
+              <Button size={"sm"} color="gray" onClick={handleClose}>
+                Cancelar
+              </Button>
+            </div>
+            <AlertDeleteAva setClodeModal={setOpen} albumInfos={albumInfos} />
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+function AlertDeleteAva({ setClodeModal, albumInfos }) {
+  const [openModal, setOpenModal] = useState(false);
+  const [avaliacoes, setAvaliacoes] = useState(false);
+  const [isAva, setIsAva] = useState(false);
+
+  useEffect(() => {
+    GetAvalAlbum(setAvaliacoes);
+  }, []);
+
+  useEffect(() => {
+    if (avaliacoes) {
+      Object.values(avaliacoes).forEach((avaliacao) => {
+        if (avaliacao.idAlbum === albumInfos.id) {
+          setIsAva(true);
+        }
+      });
+    }
+  }, [avaliacoes]);
+
+  const handleDeletAva = async () => {
+    setOpenModal(false);
+
+    const db = databaseApp;
+    const userDoc = doc(db, "avaliacoes", auth.currentUser.uid);
+
+    await updateDoc(userDoc, {
+      [`albuns.${albumInfos.id}`]: deleteField(),
+    });
+
+    await setClodeModal(false);
+  };
+
+  return (
+    <>
+      {isAva && (
+        <>
+          <Button
+            size={"sm"}
+            color="failure"
+            onClick={() => setOpenModal(true)}
+          >
+            Detelar
+          </Button>
+
+          <Modal
+            show={openModal}
+            size="md"
+            onClose={() => setOpenModal(false)}
+            popup
+          >
             <Modal.Header />
             <Modal.Body>
               <div className="text-center">
@@ -228,9 +262,8 @@ function AlertDeleteAva({setClodeModal, albumInfos}) {
               </div>
             </Modal.Body>
           </Modal>
-          
         </>
-      }
+      )}
     </>
-  )
+  );
 }
--- src/components/Modals/ModalShare.jsx
+++ src/components/Modals/ModalShare.jsx
@@ -3,6 +3,7 @@ import React, { useRef, useState } from 'react';
 import { HiInformationCircle } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import DomToImage from "dom-to-image";
+import classNames from "classnames";
 
 import FundoAva44 from '../../assets/FundoMMAvaliação442x.png'
 import FundoAva169 from '../../assets/FundoMMAvaliação.jpg'
@@ -82,59 +83,93 @@ export const ModalShare = ({ review, avaliacoesDoAlbum, ratingAlbum }) => {
   return (
     <>
       {alerta && <Alerta />}
-      <i onClick={handleOpenModal} className="fa-solid fa-up-right-from-square hover:scale-[1.15]"></i>
+      <i
+        onClick={handleOpenModal}
+        className="fa-solid fa-up-right-from-square hover:scale-[1.15]"
+      ></i>
       <Modal show={openModal} onClose={handleCloseModal}>
         <ModalHeader>Compartilhar Review do Album</ModalHeader>
         <ModalBody>
           <div className="text-white flex flex-col items-center">
             <div className="text-5xl md:text-6xl flex text-slate-300 justify-center gap-3 my-1">
-              <a className='fa-brands fa-square-twitter hover:scale-110 transition' href={`https://twitter.com/intent/tweet?text=${textShare}`} target="_blank"></a>
-              <a className="fa-brands fa-square-whatsapp hover:scale-110 transition" href={`https://wa.me/?text=${textShareWpp}`} target="_blank"></a>
+              <a
+                className="fa-brands fa-square-twitter hover:scale-110 transition"
+                href={`https://twitter.com/intent/tweet?text=${textShare}`}
+                target="_blank"
+              ></a>
+              <a
+                className="fa-brands fa-square-whatsapp hover:scale-110 transition"
+                href={`https://wa.me/?text=${textShareWpp}`}
+                target="_blank"
+              ></a>
             </div>
             <div className="w-full flex flex-col items-center">
               <div className="w-full h-12 rounded-full my-2 bg-neutral-800 flex flex-col justify-center md:items-center px-5">
                 <p className="overflow-auto">{`http://melodymingler.vercel.app/album/${review.idAlbum}`}</p>
               </div>
               <div className="flex justify-between md:justify-evenly gap-2">
-                <Button size={'sm'} onClick={handleCopy}>Copiar</Button>
-                <Link to={`http://melodymingler.vercel.app/album/${review.idAlbum}`}><Button color="gray">Seguir para o link</Button></Link>
+                <Button size={"sm"} onClick={handleCopy}>
+                  Copiar
+                </Button>
+                <Link
+                  to={`http://melodymingler.vercel.app/album/${review.idAlbum}`}
+                >
+                  <Button color="gray">Seguir para o link</Button>
+                </Link>
               </div>
-              
+
               <div className="w-full flex items-center flex-col m-2 bg-slate-600 p-3 rounded-lg">
-                <p className="text-center text-xl font-bold">Baixar uma Imagem da sua Avaliação</p>
+                <p className="text-center text-xl font-bold">
+                  Baixar uma Imagem da sua Avaliação
+                </p>
                 <div className="flex flex-col md:flex-row md:gap-2">
                   <button
-                  onClick={()=> convertDivToImage(1080, 1920)} 
-                  className="my-2 px-3 p-2 rounded-md border border-gray-400 transition-colors hover:bg-cyan-600 duration-500"
-                  >Imagem 16:9</button>
-                  
+                    onClick={() => convertDivToImage(1080, 1920)}
+                    className="my-2 px-3 p-2 rounded-md border border-gray-400 transition-colors hover:bg-cyan-600 duration-500"
+                  >
+                    Imagem 16:9
+                  </button>
+
                   <button
-                  onClick={()=> convertDivToImage(1500, 1500)} 
-                  className="my-2 px-3 p-2 rounded-md border border-gray-400 transition-colors hover:bg-cyan-600 duration-500"
-                  >Imagem 4:4</button>
+                    onClick={() => convertDivToImage(1500, 1500)}
+                    className="my-2 px-3 p-2 rounded-md border border-gray-400 transition-colors hover:bg-cyan-600 duration-500"
+                  >
+                    Imagem 4:4
+                  </button>
                 </div>
               </div>
 
-              <div ref={divRef} className={`hidden p-40 border-8 border-slate-800 items-center gap-2`}>
-                
+              <div
+                ref={divRef}
+                className={`hidden p-40 border-8 border-slate-800 items-center gap-2`}
+              >
                 <div className="w-full">
-                  <p className="text-7xl text-center font-bold mb-10">MelodyMingler</p>
+                  <p className="text-7xl text-center font-bold mb-10">
+                    MelodyMingler
+                  </p>
                   <div className="w-full flex justify-center items-center">
-                    <img className="w-[45rem] border-8 border-slate-700 rounded-xl" src={review.InfoAlbum.images[0].url}/>
+                    <img
+                      className="w-[45rem] border-8 border-slate-700 rounded-xl"
+                      src={review.InfoAlbum.images[0].url}
+                    />
                   </div>
-                  <p className="font-semibold text-7xl text-center">{review.nameAlbum}</p>
-                  <p className="text-4xl text-center">{review.InfoAlbum.artists[0].name}</p>
+                  <p className="font-semibold text-7xl text-center">
+                    {review.nameAlbum}
+                  </p>
+                  <p className="text-4xl text-center">
+                    {review.InfoAlbum.artists[0].name}
+                  </p>
 
                   <div className="flex font-bold justify-center items-center text-5xl my-1">
                     <i className="fa-solid fa-star text-[#E3A008]"></i>
                     <p>{ratingAlbum.toFixed(2)}</p>
                   </div>
-                  
-                  <p className="text-justify mt-2 text-5xl">{review.avaliacao}</p>
+
+                  <p className={classNames("text-justify mt-3", review.avaliacao.lenght < 350 ? 'text-5xl' : 'text-4xl')}>
+                    {review.avaliacao}
+                  </p>
                 </div>
-
               </div>
-
             </div>
           </div>
         </ModalBody>
